275274c31a78b751bbbb56091b0263c91856a1e9
geraldineecht <geraldineecht@gmail.com>
2023-06-30 22:09:20 -0600

    initial commit LK

---
+++ lk-wedding-invite/src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>LK Wedding</title>
+        <meta name="description" content="Lucía y Kevin" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center">
+		
+        </div>
+
+      </main>
+    </>
+  );
+}
